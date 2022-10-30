f43bc1a5aab98524ac3e76a67d7ff8e214727c24
GiftShower <hj.samuel0516@gmail.com>
2022-10-31 00:08:46 +0900

    - TS to JS transpiling on startup - Title text scroll effect

--- static/js/scroller.ts
+++ static/js/scroller.ts
@@ -1,4 +1,9 @@
 function scroller() {
-    var reveals = document.querySelectorAll(".reveal");
-    
+    const reveals = document.getElementsByClassName("codiText") as HTMLCollectionOf<HTMLElement>;
+    const current = window.scrollY;
+    console.log(current)
+    reveals.item(0).style.fontSize = `clamp(5em, calc(10em - ${current}px), 10em)`
+    reveals.item(0).style.backgroundColor = `rgba(41, 55, 78, clamp(0.7, calc(${current}/100), 1.0))`
 }
+
+window.addEventListener('scroll', scroller)
