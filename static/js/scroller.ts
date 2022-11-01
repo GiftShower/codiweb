f96a50d9c1dd1986637cbf436adba8fc175c9e56
GiftShower_ <58767497+GiftShower@users.noreply.github.com>
2022-11-01 02:50:48 +0000

    Changed CSS Styling

--- static/js/scroller.ts
+++ static/js/scroller.ts
@@ -3,7 +3,12 @@ function scroller() {
     const current = window.scrollY;
     console.log(current)
     reveals.item(0).style.fontSize = `clamp(5em, calc(10em - ${current}px), 10em)`
-    reveals.item(0).style.backgroundColor = `rgba(41, 55, 78, clamp(0.7, calc(${current}/100), 1.0))`
+    reveals.item(0).style.backgroundColor = `rgba(35, 47, 66, clamp(0.7, calc(${current}/100), 1.0))`
+}
+function clickr() {
+    window.open("https://github.com/iuicoding");
 }
 
 window.addEventListener('scroll', scroller)
+
+document.getElementsByClassName('myButton')[0].addEventListener('click', clickr)
