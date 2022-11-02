3503ee74a44fa06222ff697234f91547578e4d90
GiftShower_ <58767497+GiftShower@users.noreply.github.com>
2022-11-02 14:06:36 +0000

    wip

--- static/js/scroller.ts
+++ static/js/scroller.ts
@@ -1,7 +1,6 @@
 function scroller() {
     const reveals = document.getElementsByClassName("codiText") as HTMLCollectionOf<HTMLElement>;
     const current = window.scrollY;
-    console.log(current)
     reveals.item(0).style.fontSize = `clamp(5em, calc(10em - ${current}px), 10em)`
     reveals.item(0).style.backgroundColor = `rgba(35, 47, 66, clamp(0.7, calc(${current}/100), 1.0))`
 }
